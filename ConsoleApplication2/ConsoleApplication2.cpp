0db36e2408645fd340bae838e777676587c1687b
Dodo9531 <oge2782@mail.ru>
2023-04-13 09:15:05 +0300

    original

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,36 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+#include <iostream>
+int foo(int a, int b)
+{
+    int d = a + b;
+    return d;
+}
+int foo2(int a, int b)
+{
+    int d = a + b;
+    return d;
+}
+int arrsizes()
+{   
+    char ad[256];
+    char ds[132];
+    return sizeof(ad) * sizeof(ds);
+}
+int main()
+{
+    bool false1;
+    int a, b;
+    std::cin >> a;
+    std::cin >> b;
+    if (foo(0, 0) > 12)
+        false1 = false;
+    else
+        false1 = true;
+    false1 = false;
+    if (false1 == false)
+        std::cout << "false";
+    if (false1 == 12)
+        std::cout << "true";
+    std::cout << "Hello World!\n";
+}
